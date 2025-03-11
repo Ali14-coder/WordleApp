ef4dd513832929e33c348674e65a4ded7dacde65
Kuhle Mlinganiso <ST10259861@vcconnect.edu.za>
2025-03-12 00:29:01 +0200

    Added WhatsTheWordApi, This deals with data retrieval and sending.

--- app/src/main/java/vcmsa/projects/wordleapp/ApiService.kt
+++ app/src/main/java/vcmsa/projects/wordleapp/ApiService.kt
@@ -3,12 +3,11 @@ package vcmsa.projects.wordleapp
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.PUT
 
 interface ApiService {
 
     @POST
-    fun getGuess(guess: Guess): Call<Guess>
+    fun getGuess(): Call<Guess>
 
     @GET("CheckWord")
     fun getGuessResults(): Call<Guess>
--- app/src/main/java/vcmsa/projects/wordleapp/WhatsTheWordApi.kt
+++ app/src/main/java/vcmsa/projects/wordleapp/WhatsTheWordApi.kt
@@ -1,6 +1,41 @@
 package vcmsa.projects.wordleapp
 
+import android.content.Context
+import android.widget.Toast
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 class WhatsTheWordApi {
 
+    fun getGuess(context: Context, callback: (Guess) -> Unit){
 
+        val retrofit : Retrofit = Retrofit.Builder().baseUrl("https://whatstheword-endzgscpazgjb5c7.southafricanorth-01.azurewebsites.net/swagger/index.html").addConverterFactory(
+            GsonConverterFactory.create()).build()
+
+
+        val service: ApiService = retrofit.create<ApiService>(ApiService::class.java)
+
+        val call: Call<Guess> = service.getGuess()
+
+
+//        call.enqueue(object : Callback<Guess>{
+//            override fun onResponse(response: Response<Guess>?, retrofit: Retrofit?) {
+//
+//                if (response!!.isSuccessful){
+//                    val guess: Guess = response.body() as Guess
+//
+//                    callback(guess)
+//                }
+//            }
+//
+//            override fun onFailure(t: Throwable?) {
+//                Toast.makeText(context, "Request Fail", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+    }
 }
