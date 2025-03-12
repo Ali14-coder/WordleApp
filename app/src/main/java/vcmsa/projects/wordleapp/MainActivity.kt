568d18e0d3f3639597171435fc84d8a5c96ce6ee
Kuhle Mlinganiso <ST10259861@vcconnect.edu.za>
2025-03-13 00:15:37 +0200

    Updated the Main Activity Page to have arrays that take in the inputs from user

--- app/src/main/java/vcmsa/projects/wordleapp/MainActivity.kt
+++ app/src/main/java/vcmsa/projects/wordleapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package vcmsa.projects.wordleapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var guessFour :Array<String>
     private lateinit var guessFive :Array<String>
     private lateinit var guessSix :Array<String>
+    lateinit var btnSubmit: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,72 +30,95 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        for (i in 1..6){
 
-        guessOne = arrayOf(findViewById(R.id.r0c0),findViewById(R.id.r0c1),findViewById(R.id.r0c2),findViewById(R.id.r0c3),findViewById(R.id.r0c4))
+            when(i){
+                1 ->{
+                    guessOne = arrayOf(findViewById(R.id.r0c0),findViewById(R.id.r0c1),findViewById(R.id.r0c2),findViewById(R.id.r0c3),findViewById(R.id.r0c4))
+                    btnSubmit = findViewById(R.id.btnSubmit)
 
-        guessOne.setOnClickListener{
-            WhatsTheWordApi.getGuess(this) { _______ ->
+
+                    btnSubmit.setOnClickListener{
+                        WhatsTheWordApi().getResults(this,guessOne) { result  ->
 
 
 
+                        }
+
+                    }
+                }
+                2 ->{
+                    guessTwo = arrayOf(findViewById(R.id.r1c0),findViewById(R.id.r1c1),findViewById(R.id.r1c2),findViewById(R.id.r1c3),findViewById(R.id.r1c4))
+
+                    btnSubmit.setOnClickListener{
+                        WhatsTheWordApi().getResults(this, guessTwo) { result ->
+
+
+
+                        }
+
+                    }
+                }
+                3 ->{
+                    guessThree= arrayOf(findViewById(R.id.r2c0),findViewById(R.id.r2c1),findViewById(R.id.r2c2),findViewById(R.id.r2c3),findViewById(R.id.r2c4))
+
+                    btnSubmit.setOnClickListener{
+                        WhatsTheWordApi().getResults(this,guessThree) {  result->
+
+
+
+                        }
+
+                    }
+                }
+                4 ->{
+                    guessFour = arrayOf(findViewById(R.id.r3c0),findViewById(R.id.r3c1),findViewById(R.id.r3c2),findViewById(R.id.r3c3),findViewById(R.id.r3c4))
+
+                    btnSubmit.setOnClickListener{
+                        WhatsTheWordApi().getResults(this, guessTwo) { result ->
+
+
+
+                        }
+
+                    }
+                }
+                5 ->{
+                    guessFive = arrayOf(findViewById(R.id.r4c0),findViewById(R.id.r4c1),findViewById(R.id.r4c2),findViewById(R.id.r4c3),findViewById(R.id.r4c4))
+
+                    btnSubmit.setOnClickListener{
+                        WhatsTheWordApi().getResults(this, guessTwo) { result ->
+
+
+
+                        }
+
+                    }
+                }
+                6 ->{
+                    guessSix = arrayOf(findViewById(R.id.r5c0),findViewById(R.id.r5c1),findViewById(R.id.r5c2),findViewById(R.id.r5c3),findViewById(R.id.r5c4))
+
+                    btnSubmit.setOnClickListener{
+                        WhatsTheWordApi().getResults(this, guessTwo) { result ->
+
+
+
+                        }
+
+                    }
+                }
             }
 
         }
 
-        guessTwo = arrayOf(findViewById(R.id.r1c0),findViewById(R.id.r1c1),findViewById(R.id.r1c2),findViewById(R.id.r1c3),findViewById(R.id.r1c4))
-
-        guessOne.setOnClickListener{
-            WhatsTheWordApi.getGuess(this) { _______ ->
 
 
 
-            }
-
-        }
-
-        guessThree= arrayOf(findViewById(R.id.r2c0),findViewById(R.id.r2c1),findViewById(R.id.r2c2),findViewById(R.id.r2c3),findViewById(R.id.r2c4))
-
-        guessOne.setOnClickListener{
-            WhatsTheWordApi.getGuess(this) { _______ ->
 
 
 
-            }
-
-        }
-
-        guessFour = arrayOf(findViewById(R.id.r3c0),findViewById(R.id.r3c1),findViewById(R.id.r3c2),findViewById(R.id.r3c3),findViewById(R.id.r3c4))
-
-        guessOne.setOnClickListener{
-            WhatsTheWordApi.getGuess(this) { _______ ->
 
 
-
-            }
-
-        }
-
-        guessFive = arrayOf(findViewById(R.id.r4c0),findViewById(R.id.r4c1),findViewById(R.id.r4c2),findViewById(R.id.r4c3),findViewById(R.id.r4c4))
-
-        guessOne.setOnClickListener{
-            WhatsTheWordApi.getGuess(this) { _______ ->
-
-
-
-            }
-
-        }
-
-        guessSix = arrayOf(findViewById(R.id.r5c0),findViewById(R.id.r5c1),findViewById(R.id.r5c2),findViewById(R.id.r5c3),findViewById(R.id.r5c4))
-
-        guessOne.setOnClickListener{
-            WhatsTheWordApi.getGuess(this) { _______ ->
-
-
-
-            }
-
-        }
 
 
 
